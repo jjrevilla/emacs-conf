c0cfdcba4beee5df3851c5e5ab916a37cc68c6c6
Jair Revilla <jair.revilla@ucsp.edu.pe>
2017-12-17 19:57:22 -0500

    Add new init file

---
+++ init.el
@@ -0,0 +1,37 @@
+;;; init.el --- my Emacs Init File
+;; Author: Jair Revilla <jair.revilla@gmail.com>
+;; Keywords: Emacs, dotfile, config
+
+;;; Code:
+;; This file is only provided as an example. Customize it to your own taste!
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(delete-selection-mode t)
+ '(fill-column 80)
+ '(flycheck-highlighting-mode (quote symbols))
+ '(global-auto-revert-mode t)
+ '(global-font-lock-mode t)
+ '(global-linum-mode t)
+ '(prefer-coding-system (quote utf-8))
+ '(show-paren-delay 0)
+ '(make-backup-files nil)
+ '(indent-tabs-mode nil)
+ '(show-paren-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 90 :width normal)))))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu"   . "http://elpa.gnu.org/packages/"))
+(package-initialize)
